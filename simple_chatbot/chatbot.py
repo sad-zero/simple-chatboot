c87cfb06a70bbea5e46661639fc61c6d7ff02e3e
sad-zero <zeroro.yun@gmail.com>
2024-05-19 21:43:47 +0900

    feat: in-memory histories

--- simple_chatbot/chatbot.py
+++ simple_chatbot/chatbot.py
@@ -9,9 +9,14 @@ from chromadb import Collection
 from langchain_core.embeddings.embeddings import Embeddings
 from langchain_core.language_models.llms import BaseLLM
 from langchain_core.messages import BaseMessage, SystemMessage, HumanMessage, AIMessage
-from langchain_core.prompts import ChatPromptTemplate, HumanMessagePromptTemplate
+from langchain_core.prompts import ChatPromptTemplate, HumanMessagePromptTemplate, MessagesPlaceholder
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import Runnable, RunnablePassthrough
+from langchain.memory import ConversationBufferMemory
+from langchain.memory.chat_memory import BaseChatMemory
+from langchain_core.runnables.history import RunnableWithMessageHistory
+from langchain_core.chat_history import BaseChatMessageHistory
+from langchain_community.chat_message_histories.in_memory import ChatMessageHistory
 
 
 class _Memory:
@@ -75,43 +80,59 @@ class _CustomRetriever(BaseRetriever):
 class Chatbot:
     __model: BaseLLM
     __retreiver: BaseRetriever
-    __memory: _Memory
+    __sessions = {}
+    __rules: List[SystemMessage] = []
     __rag_template = HumanMessagePromptTemplate.from_template(template="[Data]\n{data}\n[Question]\n{query}")
 
     def __init__(self, model: BaseLLM, embeddings: Embeddings, db: Collection, top_k: int = 5, memory_size: int = 20):
         self.__model = model
         self.__retreiver = _CustomRetriever(embeddings=embeddings, db=db, top_k=top_k)
-        self.__memory = _Memory(limit=memory_size)
 
     def add_rule(self, rule: str):
-        self.__memory.append_core_message(SystemMessage(content=rule))
+        self.__rules.append(SystemMessage(content=rule))
 
-    def chat(self, query: str) -> str:
+    def chat(self, query: str, session_id: str = "test_session") -> str:
         """
         Memory 필요
         """
-        core_messages = self.__memory.get_core()
-        chat_messages = self.__memory.get_chat()
-        messages = core_messages + chat_messages + [self.__rag_template]
+        # rule -> histories -> current chat 순서로 모델에 입력된다.
+        messages = [*self.__rules, MessagesPlaceholder(variable_name="history"), self.__rag_template]
         prompt = ChatPromptTemplate.from_messages(messages=messages)
 
         chain: Runnable = self.__get_chain(prompt=prompt)
-        result = chain.invoke(query)
 
-        query = self.__rag_template.format(data=self.__retreiver.invoke(query), query=query)
-        self.__memory.append_chat_message(query)
-        self.__memory.append_chat_message(AIMessage(content=result))
+        with_message_history = RunnableWithMessageHistory(
+            runnable=chain,
+            get_session_history=self.__get_session_history,
+            input_messages_key="query",
+            history_messages_key="history",
+        )
 
-        chat_histories = core_messages + chat_messages + [query, result]
+        def format_docs(documents: List[Document]):
+            return "\n\n".join(map(lambda x: x.page_content, documents))
+
+        result = with_message_history.invoke(
+            input={"data": (self.__retreiver | format_docs).invoke(query), "query": query},
+            config={"configurable": {"session_id": session_id}},
+        )
+        chat_histories = self.__get_session_history(session_id).messages
         logging.debug(f"{chat_histories}")
 
+        return result
+
+    def __get_session_history(self, session_id: str) -> BaseChatMessageHistory:
+        if session_id not in self.__sessions:
+            self.__sessions[session_id] = ChatMessageHistory()
+        result: ChatMessageHistory = self.__sessions[session_id]
+        result.add_user_message(HumanMessage(content="안녕하세요"))
+        result.add_ai_message(AIMessage(content="반갑습니다"))
         return result
 
     def query(self, query: str) -> str:
         """
         단발성 질문
         """
-        prompt = ChatPromptTemplate.from_messages(self.__memory.get_core() + [self.__rag_template])
+        prompt = ChatPromptTemplate.from_messages(self.__rules + [self.__rag_template])
 
         chain: Runnable = self.__get_chain(prompt=prompt)
         result = chain.invoke(query)
@@ -122,13 +143,5 @@ class Chatbot:
         return result
 
     def __get_chain(self, prompt: ChatPromptTemplate) -> Runnable:
-        def format_docs(documents: List[Document]):
-            return "\n\n".join(map(lambda x: x.page_content, documents))
-
-        chain: Runnable = (
-            {"data": self.__retreiver | format_docs, "query": RunnablePassthrough()}
-            | prompt
-            | self.__model
-            | StrOutputParser()
-        )
+        chain: Runnable = prompt | self.__model | StrOutputParser()
         return chain
